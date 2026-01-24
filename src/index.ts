6a4b278d53720e2b03c93c029a20a56d7b9382ef
ganrLoya <genarodour210@gmail.com>
2026-01-23 23:16:52 -0600

    basic implemntation fro stream in ffmpeg

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { spawn } from "node:child_process";
 import path from "node:path";
 import express from "express";
 
@@ -5,30 +6,50 @@ const app = express();
 const PORT = 3000;
 const cwd = process.cwd();
 
-// Enable CORS for all requests
-app.use((_req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Range");
-  next();
+app.use("hls", express.static(path.join(cwd, ".data", "media")));
+
+app.get("/,video-web", (_req, res) => {
+  res.sendFile(path.join(cwd, "public", "index.html"));
 });
 
-app.use(
-  "/hls",
-  express.static(path.join(cwd, ".data", "media"), {
-    setHeaders: (res, filePath) => {
-      const ext = path.extname(filePath);
-      if (ext === ".m3u8") {
-        res.setHeader("Content-Type", "application/vnd.apple.mpegurl");
-      } else if (ext === ".ts") {
-        res.setHeader("Content-Type", "video/mp2t");
-      }
-    },
-  }),
-);
+app.get("/live", (req, res) => {
+  const videoPath = path.join(cwd, ".data", "media", "ForBiggerEscapes.mp4");
 
-app.get("/video-web", (_req, res) => {
-  res.sendFile(path.join(cwd, "public", "index.html"));
+  // Lanzamos FFmpeg para crear el stream en tiempo real
+  const ffmpeg = spawn("ffmpeg", [
+    "-re", // reproducir a velocidad real
+    "-i",
+    videoPath, // input
+    "-c:v",
+    "libx264", // video
+    "-c:a",
+    "aac", // audio
+    "-f",
+    "mpegts", // contenedor compatible con VLC/ffplay
+    "pipe:1", // salida al stdout
+  ]);
+
+  res.writeHead(200, {
+    "Content-Type": "video/mp2t",
+  });
+
+  ffmpeg.stdout.on("data", (chunk) => {
+    console.log(`Enviando chunk de ${chunk.length} bytes`);
+  });
+
+  ffmpeg.stdout.pipe(res);
+
+  // Si el cliente se desconecta, matamos FFmpeg
+  req.on("close", () => {
+    console.log("Cleint disconnection");
+    ffmpeg.kill();
+  });
+
+  // Cuando FFmpeg termina, cerramos la respuesta HTTP
+  ffmpeg.on("close", () => {
+    console.log("FFmpeg closed");
+    res.end();
+  });
 });
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
